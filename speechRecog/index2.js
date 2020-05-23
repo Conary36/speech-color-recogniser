1fa93a5a5307ded2090db77047498b4b26574614
Conary36 <cbeckford144@gmail.com>
2020-05-23 12:10:45 -0400

    index2.js collecting sounds

--- speechRecog/index2.js
+++ speechRecog/index2.js
@@ -1,11 +1,23 @@
-async function app() {
+
+async function collectSounds(recognizer, name, count){
+    for(let i = 0; i < count; i++){
+        console.log(`Say ${name}`);
+        await recognizer.collectExample(name);
+    }
+}
+
+
+
+async function speech() {
     let baseRecognizer = speechCommands.create('BROWSER_FFT');
     console.log("Model Loaded....");
     await baseRecognizer.ensureModelLoaded();
     console.log("Creating Transfer recognizer....");
     const transferRecognizer = baseRecognizer.createTransfer('colors');
-    console.log("Collecting examples....");
-    await transferRecognizer.collectExample('Red');
+    await collectSounds(transferRecognizer, "red", 10)
+    await collectSounds(transferRecognizer, "_background_noise_", 10)
+    console.log("Finished...");
+
 }
 
-app();
+speech();
