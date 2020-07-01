bf9baea4b718c3d036d6539971aec43caae8f526
Conary36 <cbeckford144@gmail.com>
2020-07-01 13:03:25 -0400

    modified code index2.js & saving to voyager.pages

--- speechRecog/index2.js
+++ speechRecog/index2.js
@@ -1,6 +1,7 @@
+
 const exampleCount = 2;
 
-async function collectSounds(recognizer, name, count){
+ async function collectSounds(recognizer, name, count){
     for(let i = 0; i < count; i++){
         console.log(`Say ${name}`);
         await recognizer.collectExample(name);
@@ -15,40 +16,41 @@ function download(transferRecognizer) {
 
     // Trigger downloading of the data .bin file.
     const anchor = document.createElement('a');
-    anchor.download = `blazing.bin`;
+    anchor.download = `voyager.pages`;
     anchor.href = window.URL.createObjectURL(
         new Blob([artifacts], { type: 'application/octet-stream' }));
     anchor.click();
 };
 
 
-async function createTrainModel(transferRecognizer){
-    await collectSounds(transferRecognizer, "OK Finder", exampleCount)
-    //await collectSounds(transferRecognizer, "Hey Brave", exampleCount)
-    //await collectSounds(transferRecognizer, "Open Search", exampleCount)
-    await collectSounds(transferRecognizer, "_background_noise_", exampleCount)
-    console.log("Finished...");
+// async function createTrainModel(transferRecognizer){
+//     await collectSounds(transferRecognizer, "OK Finder", exampleCount)
+//     //await collectSounds(transferRecognizer, "Hey Brave", exampleCount)
+//     //await collectSounds(transferRecognizer, "Open Search", exampleCount)
+//     await collectSounds(transferRecognizer, "_background_noise_", exampleCount)
+//     console.log("Finished...");
 
-    console.log(transferRecognizer.countExamples());
+//     console.log(transferRecognizer.countExamples());
 
-    // Start training of the transfer-learning model.
-    // You can specify `epochs` (number of training epochs) and `callback`
-    // (the Model.fit callback to use during training), among other configuration
-    // fields.
-    console.log("Training....")
-    await transferRecognizer.train({
-        epochs: 25,
-        callback: {
-            onEpochEnd: async (epoch, logs) => {
-                console.log(`Epoch ${epoch}: loss=${logs.loss}, accuracy=${logs.acc}`)
-            }
-        }
-    });
-}
+//     // Start training of the transfer-learning model.
+//     // You can specify `epochs` (number of training epochs) and `callback`
+//     // (the Model.fit callback to use during training), among other configuration
+//     // fields.
+//     console.log("Training....")
+//     await transferRecognizer.train({
+//         epochs: 25,
+//         callback: {
+//             onEpochEnd: async (epoch, logs) => {
+//                 console.log(`Epoch ${epoch}: loss=${logs.loss}, accuracy=${logs.acc}`)
+//             }
+//         }
+//     });
 
-async function loadModel(transferRecognizer){
 
-}
+// }
+
+
+
 
 
 async function speech() {
@@ -58,16 +60,28 @@ async function speech() {
 
     console.log("Creating Transfer recognizer....");
     const transferRecognizer = baseRecognizer.createTransfer('helper');
-    await loadModel(transferRecognizer);
-   
+    await collectSounds(transferRecognizer, "Ok Finder", exampleCount);
+    await collectSounds(transferRecognizer, "_background_noise_", exampleCount)
+    console.log("Finished...");
+
+    console.log(transferRecognizer.countExamples());
+
     
 
-    // console.log("Downloading...")
-    // download(transferRecognizer);
+    console.log("Training....")
+    await transferRecognizer.train({
+        epochs: 25,
+        callback: {
+            onEpochEnd: async (epoch, logs) => {
+                console.log(`Epoch ${epoch}: loss=${logs.loss}, accuracy=${logs.acc}`)
+            }
+        }
+    });
+    console.log("Downloading....");
+    download(transferRecognizer)
 
-    // After the transfer learning completes, you can start online streaming
-    // recognition using the new model.
-    console.log("Listening....")
+    
+    console.log("Listening....");
     await transferRecognizer.listen(result => {
         // - result.scores contains the scores for the new vocabulary, which
         //   can be checked with:
@@ -77,19 +91,14 @@ async function speech() {
          scores = Array.from(result.scores).map((s, i) => ({ score: s, word: words[i] }));
          // Find the most probable word.
          scores.sort((s1, s2) => s2.score - s1.score);
-
-        // `result.scores` contains the scores for the new words, not the original
-        // words.
-        //for (let i = 0; i < words.length; ++i) {//words is an array of words...SO I ADDED .LENGTH
-            console.log(`score for word '${scores[0].word}' = ${scores[0].score}`);
-        //}
-        console.log("------------------------------------------------------")
+         console.log(`score for word '${scores[0].word}' = ${scores[0].score}`);
+         console.log("------------------------------------------------------")
     }, { probabilityThreshold: 0.75 });
 
    
 
-    // Stop the recognition in 10 seconds.
-    setTimeout(() => transferRecognizer.stopListening(), 20e3);
+    // Stop the recognition in 60 seconds.
+    setTimeout(() => transferRecognizer.stopListening(), 60e3);
     
 }
 
